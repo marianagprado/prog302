84eed896d99b7d0752ba9fe1f659509b415a2e7c
marianagprado <122393144+marianagprado@users.noreply.github.com>
2023-02-23 10:09:26 -0300

    Update classe_pessoa.py

--- classe_pessoa.py
+++ classe_pessoa.py
@@ -1,1 +1,11 @@
+class Pessoa:
+    # usando valor padrão nos parâmetros do construtor
+    def __init__(self, nome="", email="", telefone=""):
+        self.nome = nome
+        self.email = email
+        self.telefone = telefone
 
+    # usando string formatada, que coisa linda :-)
+    def __str__(self):
+        return f'{self.nome}, '+\
+               f'{self.email}, {self.telefone}'
