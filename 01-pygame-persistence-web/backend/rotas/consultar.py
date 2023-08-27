357c9f9aaa52528b2c69b8624efd3c9ca0086cb9
marianagprado <122393144+marianagprado@users.noreply.github.com>
2023-08-27 18:41:25 -0300

    Add files via upload

---
+++ 01-pygame-persistence-web/backend/rotas/consultar.py
@@ -0,0 +1,53 @@
+from backend.geral.config import *
+from backend.modelo.modelo import *
+
+@app.route("/consultar/<string:classe>/<int:id>")
+def consultar(classe, id):
+    try:    
+      # obter o objeto
+      obj = None
+      if classe == "Cena":
+          obj = db.session.get(Cena, id)
+
+      # achou?        
+      if obj:
+        meujson = {"resultado": "ok"}
+        meujson.update({"detalhes": obj.json()})
+        return jsonify(meujson)
+      else:
+        return jsonify({"resultado":"erro", "detalhes":"objeto não encontrado: "+id})
+      
+    except Exception as e:
+        return jsonify({"resultado": "erro", "detalhes": str(e)})    
+
+
+@app.route("/consultar/<string:classe>/<int:id>")
+def consultar(classe, id):
+    try:    
+      # obter o objeto
+      obj = None
+      if classe == "Jogador":
+          obj = db.session.get(Jogador, id)
+
+      # achou?        
+      if obj:
+        meujson = {"resultado": "ok"}
+        meujson.update({"detalhes": obj.json()})
+        return jsonify(meujson)
+      else:
+        return jsonify({"resultado":"erro", "detalhes":"objeto não encontrado: "+id})
+      
+    except Exception as e:
+        return jsonify({"resultado": "erro", "detalhes": str(e)})    
+
+
+    '''
+exemplo de teste:
+curl localhost:5000/atualizar/Jogador -d '{"id":1, "x":150}' -H 'Content-Type:application/json' -X POST
+{
+  "detalhes": "ok",
+  "resultado": "ok"
+}
+
+
+'''
---
+++ 01-pygame-persistence-web/backend/testes/TestarJogo.py
@@ -0,0 +1,18 @@
+from backend.geral.config import *
+from backend.modelo.modelo import *
+
+def run():
+    print("TESTE DE CENA")
+    
+    c2= Cena ( obstaculos= "300,1,30,170|420,1,30,100|540,1,30,200|660,1,30,120|300,400,30,200|420,300,30,350|540,400,30,250|660,300,30,400")
+    c1= Cena ( obstaculos= "300,1,30,230|420,1,30,70|540,1,30,280|660,1,30,100|300,400,30,280|420,300,30,400|540,400,30,180|660,300,30,300")
+    c3= Cena ( obstaculos= "300,1,30,100|420,1,30,150|540,1,30,320|660,1,30,150|300,400,30,240|420,300,30,300|540,400,30,220|660,300,30,450")
+
+    db.session.add(c1)
+    db.session.add(c2)
+    db.session.add(c3)
+    db.session.commit()
+
+    print(c1)
+    print(c2)
+    print(c3.json())
